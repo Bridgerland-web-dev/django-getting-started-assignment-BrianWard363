9441561e5ff660dc8110ab1a1c3770656f69a328
landonkb <92946187+landonkb@users.noreply.github.com>
2023-02-15 16:55:36 -0700

    part 6 finished

--- real/demos/before/meeting_planner/meeting_planner/urls.py
+++ real/demos/before/meeting_planner/meeting_planner/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from website.views import welcome, date, about
 from meetings.views import detail
@@ -22,8 +22,9 @@ from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome),
+    path('', welcome, name="welcome"),
     path('date', date),
     path('about', about),
-    path('meetings/<int:id>', detail),
+    path('detail', detail),
+    path('meetings/', include('meetings.urls')),
 ]
--- real/demos/before/meeting_planner/website/views.py
+++ real/demos/before/meeting_planner/website/views.py
@@ -3,14 +3,15 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from meetings.models import Meeting
+
 
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
-
+    return render(request, "website/welcome.html",
+                   {"meetings": Meeting.objects.all()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
-
 
 # Please add: An about page that shows some text about yourself
 def about(request):
